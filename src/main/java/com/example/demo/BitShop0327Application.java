409396502f64631b59104ca7350edce648b7dea8
sajh78 <redksa0708@gmail.com>
2020-03-27 12:45:45 +0900

    sts aop 연습 시작

---
+++ src/main/java/com/example/demo/BitShop0327Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitShop0327Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitShop0327Application.class, args);
+	}
+
+}
